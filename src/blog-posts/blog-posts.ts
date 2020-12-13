eac995a69b594e9ae7fb66be71794ce4e1f073b9
s-fletcher <sfletchertx@gmail.com>
2020-12-12 18:39:41 -0600

    Streamline new posts

---
+++ src/blog-posts/blog-posts.ts
@@ -0,0 +1,26 @@
+export type BlogPost = {
+    title: string;
+    date: string;
+    image: string;
+    contentUrl: string;
+}
+
+/**
+ * These are the individual blog posts.
+ */
+const blogPosts: BlogPost[] = [
+  {
+    title:      'This is an Example Blog Post',
+    date:       'December 12, 2020',
+    image:      require('./images/example-image.png'),
+    contentUrl: require('./example-blog-post.md'),
+  },
+  {
+    title:      'This is an Example Blog Post 2',
+    date:       'December 16, 2020',
+    image:      require('./images/example-image.png'),
+    contentUrl: require('./example-blog-post.md'),
+  },
+];
+
+export default blogPosts;
--- src/views/Blog.tsx
+++ src/views/Blog.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/blog.scss';
+// import marked from 'marked';
+import blogPosts, { BlogPost } from '../blog-posts/blog-posts';
+
+type PostPreview = {
+  title: string;
+  date: string;
+  image: string;
+  preview: string;
+}
 
 /**
- * Blog
+ * Gallery of blog posts
  */
-const Blog: React.FC = () => (
-  <div id="blog">
-    <div className="container">
-      <div className="content">
-        <p>Blog</p>
+const Blog: React.FC = () => {
+  const [postData, setPostData] = useState<PostPreview[]>([]);
+
+  useEffect(() => {
+    /**
+     * Get post data
+     */
+    async function getPostData(post: BlogPost): Promise<void> {
+      let preview = 'test';
+
+      await fetch(post.contentUrl)
+        .then((res) => res.text())
+        .then((text) => {
+          preview = `${text.substr(0, 200).replace('# ', '')}...`;
+        });
+
+      const newData = postData.concat({
+        ...post,
+        preview,
+      });
+
+      setPostData(newData);
+    }
+
+    if (blogPosts[postData.length]) {
+      getPostData(blogPosts[postData.length]);
+    }
+  }, [postData]);
+
+  /**
+   * Renders a preview for a single blog post
+   */
+  const renderPostPreview = (post: PostPreview): JSX.Element => (
+    <div key={post.title}>
+      <p>{post.title}</p>
+      <p>{post.date}</p>
+      <img src={post.image} alt={post.title} />
+      {/* eslint-disable-next-line react/no-danger */}
+      <div dangerouslySetInnerHTML={{ __html: post.preview }} />
+    </div>
+  );
+
+  return (
+    <div id="blog">
+      <div className="container">
+        {postData.map((post) => renderPostPreview(post))}
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 export default Blog;
