962798b39502259b5ab0bd88e15186310275c2f5
carlyburwell <75957509+carlyburwell@users.noreply.github.com>
2020-12-13 19:46:12 -0600

    Update blog-posts.ts

--- src/blog-posts/blog-posts.ts
+++ src/blog-posts/blog-posts.ts
@@ -15,7 +15,7 @@ const blogPosts: BlogPostProps[] = [
     date:       'December 13, 2020',
     image:      require('./images/welcome.png'),
     contentUrl: require('./welcome.md'),
-    preview:    'Learn about my life, my passions, and my goals. I am so happy you are here, feel free to stay awhile!',
+    preview:    'Learn about my life, my passions, and my goals. I am so happy you are here, feel free to stay for a while!',
   },
   {
     title:      'This is an Example Blog Post This is a long one',
