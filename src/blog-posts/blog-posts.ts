d5a553a63c676ca89b756fa6b64f33e942603437
s-fletcher <sfletchertx@gmail.com>
2020-12-13 20:21:56 -0600

    Fix image errors

--- src/blog-posts/blog-posts.ts
+++ src/blog-posts/blog-posts.ts
@@ -13,22 +13,22 @@ const blogPosts: BlogPostProps[] = [
   {
     title:      'Welcome to my Blog!',
     date:       'December 13, 2020',
-    image:      require('./images/welcomeheader.png'),
-    contentUrl: require('./welcome.md'),
+    image:      './images/welcomeheader.png',
+    contentUrl: './welcome.md',
     preview:    'Learn about my life, my passions, and my goals. I am so happy you are here, feel free to stay a while!',
   },
   {
     title:      'This is an Example Blog Post This is a long one',
     date:       'December 16, 2020',
-    image:      require('./images/example-image.png'),
-    contentUrl: require('./example-blog-post.md'),
+    image:      './images/example-image.png',
+    contentUrl: './example-blog-post.md',
     preview:    'lorem espion',
   },
   {
     title:      'This is an Example Blog Post 2',
     date:       'December 16, 2020',
-    image:      require('./images/example-image.png'),
-    contentUrl: require('./example-blog-post.md'),
+    image:      './images/example-image.png',
+    contentUrl: './example-blog-post.md',
     preview:    'lorem espion',
   },
 ];
